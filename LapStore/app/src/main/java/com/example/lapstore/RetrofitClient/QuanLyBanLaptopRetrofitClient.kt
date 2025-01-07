a618c96dd61168b7c4da204556a78e0954c72ad8
Oanh1503-git <132774263+Oanh1503-git@users.noreply.github.com>
2025-01-07 07:48:15 +0700

    comment khach hang

--- LapStore/app/src/main/java/com/example/lapstore/RetrofitClient/QuanLyBanLaptopRetrofitClient.kt
+++ LapStore/app/src/main/java/com/example/lapstore/RetrofitClient/QuanLyBanLaptopRetrofitClient.kt
@@ -18,4 +18,22 @@ object QuanLyBanLaptopRetrofitClient {
             .build()
             .create(SanPhamAPIService::class.java)
     }
+<<<<<<< Updated upstream
+=======
+
+    val hinhAnhAPIService: HinhAnhAPIService by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+            .create(HinhAnhAPIService::class.java)
+    }
+//    val khachHangAPIService: KhachHangAPIService by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(Constants.BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+//            .build()
+//            .create(KhachHangAPIService::class.java)
+//    }
+>>>>>>> Stashed changes
 }
