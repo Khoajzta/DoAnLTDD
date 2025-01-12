9406d67567dc503c2dc0d8989859711f25deea6d
Khoajzta <101576530+Khoajzta@users.noreply.github.com>
2025-01-12 18:55:53 +0700

    update chức năng and api

--- LapStore/LapStore/app/src/main/java/com/example/lapstore/models/HinhAnh.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lapstore.models
-
-data class HinhAnh(
-    var MaHinhAnh: Int,
-    var DuongDan:String,
-    var MaSanPham:Int
-)
--- LapStore/LapStore/app/src/main/java/com/example/lapstore/views/Admin_Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lapstore.views
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun AdminScreen(){
-    
-}
